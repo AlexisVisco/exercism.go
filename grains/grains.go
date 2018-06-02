cee2bf1fce806dd4c3cfe140fa8eee8d1f26b34a
alexisvisco <alexis.viscogliosi@outlook.fr>
2018-06-02 18:29:09 +0200

    resolve grains

---
+++ grains/grains.go
@@ -0,0 +1,24 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+// Sum of a geometric suite of U(n+1)=2*U(n) == 2^(64+1) - 1.
+// 2^(64+1) - 1 == 3.68934881e19 == 36899348810000000000
+// 36899348810000000000 > math.MaxUint64
+// value of this exercise cannot be performed in despite of
+// the architecture of our computer and how go has implemented
+// integers.
+// Since we must return something, we must return what we are able to return.
+func Total() uint64 {
+	return math.MaxUint64
+}
+
+func Square(n int) (uint64, error) {
+	if n <= 0 || n > 64 {
+		return 0, errors.New("negative greather than 64")
+	}
+	return 1 << uint64(n-1), nil
+}
